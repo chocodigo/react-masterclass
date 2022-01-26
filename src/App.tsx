a2f8ab43cb8b6a759c464391041dba6659335145
chocodigo <cocoa1149@gmail.com>
2022-01-26 19:05:39 +0900

    3.5 Forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import styled, { keyframes } from "styled-components";
-import Circle from "./Circle";
+import React, { useState } from "react";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setUsername(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", username);
+  };
+
   return (
     <div>
-      <Circle borderColor={"yellow"} bgColor={"teal"} />
-      <Circle text={"hi"} bgColor={"tomato"} />
+      <form onSubmit={onSubmit}>
+        <input
+          value={username}
+          onChange={onChange}
+          type={"text"}
+          placeholder={"username"}
+        />{" "}
+        <button>Log in</button>{" "}
+      </form>
     </div>
   );
 }
